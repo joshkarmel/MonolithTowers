4d09d121b206313c9865da94559ec1b7d5fae57a
Joshua Karmel <callmeshua620@aim.com>
2025-02-11 17:11:53 -0500

    initialize unreal project
    initialize unreal project

---
+++ Intermediate/Build/Win64/x64/MonolithTowersEditor/Development/Engine/SharedPCH.Engine.Cpp17.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32548
+#include "C:/Users/Shua/Documents/Unreal Projects/MonolithTowers/Intermediate/Build/Win64/x64/MonolithTowersEditor/Development/Engine/SharedPCH.Engine.Cpp17.h"
---
+++ Source/MonolithTowers/MonolithTowers.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MonolithTowers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MonolithTowers, "MonolithTowers" );
