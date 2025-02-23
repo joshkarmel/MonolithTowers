307baf419cde3e64a398d13531dc8802ae2bb86e
Joshua Karmel <callmeshua620@aim.com>
2025-02-22 21:40:26 -0500

    extra nonsense
    idk

--- Intermediate/Build/BuildRulesProjects/MonolithTowersModuleRules/obj/Development/MonolithTowersModuleRules.AssemblyInfo.cs
+++ Intermediate/Build/BuildRulesProjects/MonolithTowersModuleRules/obj/Development/MonolithTowersModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonolithTowersModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644aa1853844ec5a7ca274331e1e88ba7b1c0789")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonolithTowersModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonolithTowersModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
